b5a9fd873725d2db0e3285f6d13eabb5227c0bbb
winniemwai <winniemwai5451@gmail.com>
2024-12-16 17:50:32 +0300

    added more JavaScript details

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "2 ⅔ cups all-purpose flour, or more as needed",
-    autoStart: true,
-    delay: 2,
-    cursor: "",
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "o43a8f57ft70fd8b00ada9b587fc3111";
+  let context =
+    " rou are a cake  expert who knows many types of recipes.Your mission is to generate a simple recipe in basic HTML format and separate each line with <br/>.Make sure to follow the user instruction.You can include a title to the recipe.";
+  let prompt = `User instructions: Generate a recipe on ${instructionsInput.value}`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}
+    `;
+
+  let recipeElement = document.querySelector("#recipe");
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
