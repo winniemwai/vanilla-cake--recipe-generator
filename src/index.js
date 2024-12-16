174d0d20b5f2f60f39ea022952b23c2c5743f34d
winniemwai <winniemwai5451@gmail.com>
2024-12-16 15:07:16 +0300

    imported Type Writer plugging and Java Script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "2 ⅔ cups all-purpose flour, or more as needed",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
